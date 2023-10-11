f46ad9b5e91f9b54cf882259134aa8568720b6fb
cakrawrz_ <cakradwi1@gmail.com>
2023-10-12 00:30:03 +0700

    update

---
+++ app/src/main/java/com/example/macrochallenge9/ScrollPopulerWisata.kt
@@ -0,0 +1,11 @@
+package com.example.macrochallenge9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollPopulerWisata : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll_populer_wisata)
+    }
+}
