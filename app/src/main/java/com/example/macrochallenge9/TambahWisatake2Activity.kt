924604117b0d5f959b62371967dfd2d8fe8cdad3
cakrawrz_ <cakradwi1@gmail.com>
2023-10-18 23:48:49 +0700

    update profil

---
+++ app/src/main/java/com/example/macrochallenge9/TambahWisatake2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.macrochallenge9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TambahWisatake2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_wisatake2)
+    }
+}
